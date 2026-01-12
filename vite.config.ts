f665f5fdd8dc1445ad56fc54d3d46ef8cfbae7f0
melserngl <128845117+nglmercer@users.noreply.github.com>
2026-01-11 20:26:01 -0500

    fix basepath

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
+    base: "/trigger-workflow/",
     server: {
       port: 3000,
       host: "0.0.0.0",
